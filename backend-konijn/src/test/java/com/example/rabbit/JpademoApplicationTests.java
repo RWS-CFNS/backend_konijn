6498b79b02d78ceac656c01475c88833a886d3c3
nielsnew <nielsadebruin@hotmail.com>
2023-09-22 15:53:56 +0200

    added junit test for JPA database H2

--- backend-konijn/src/test/java/com/example/rabbit/JpademoApplicationTests.java
+++ backend-konijn/src/test/java/com/example/rabbit/JpademoApplicationTests.java
@@ -3,29 +3,35 @@ package com.example.rabbit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class JpademoApplicationTests {
-//
-//   @Autowired
-//   MeasuringboxRepository userRepository;
-//
-//   @Test
-//   public void contextLoads() {
-//
-//	   	Measuringbox boxMeasuringbox = new Measuringbox("testje", 5, 2);
-//        Measuringbox user = MeasuringboxRepository.save(boxMeasuringbox);
-//        Measuringbox searchUser= MeasuringboxRepository.findById(user.get());
-//
-//        assertNotNull(searchUser);
-//        assertEquals(user.getId(),searchUser.getId());
-//
-//   }
-//
-//}
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpademoApplicationTests {
+
+   @Autowired
+   MeasuringboxRepository boxRepository;
+   
+   @Autowired
+   MeasuringboxService boxService;
+
+   @Test
+   public void contextLoads() {
+
+	   	Measuringbox boxMeasuringbox = new Measuringbox("testje", 5, 2); //create box example
+	   	boxRepository.save(boxMeasuringbox); //put box in database
+        Optional<Measuringbox> thisBoxIsSus = boxRepository.findById(boxMeasuringbox.getId()); //find box in database using ID
+        Measuringbox definatelyABox = thisBoxIsSus.get(); //check if box is not null, convert it from "optional"  to normal object
+        
+        assertNotNull(definatelyABox); //check if box is not null
+        assertEquals(boxMeasuringbox.getId(),definatelyABox.getId()); //compare ID box example, with ID in the box from the database
+
+   }
+
+}
--- backend-konijn/src/test/java/com/example/rabbit/TestBackendKonijnApplication.java
+++ backend-konijn/src/test/java/com/example/rabbit/TestBackendKonijnApplication.java
@@ -1,5 +1,6 @@
 package com.example.rabbit;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
