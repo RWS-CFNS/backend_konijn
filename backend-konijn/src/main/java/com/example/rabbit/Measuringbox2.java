f362ec520b53e9f3e3ce1e6732253f5025691ea2
nielsnew <nielsadebruin@hotmail.com>
2023-09-25 15:04:17 +0200

    demo second full measuringbox

---
+++ backend-konijn/src/main/java/com/example/rabbit/Measuringbox2.java
@@ -0,0 +1,32 @@
+package com.example.rabbit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "MTABLE")
+public class Measuringbox2 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "BOX_NAME", length = 50, nullable = false, unique = false)
+	private String name;
+
+	@Transient
+	private Integer value1;
+
+
+
+	// @Enumerated(EnumType.STRING)
+	// private boolean measuringboxGender;
+
+	// other fields, getters and setters
+}
