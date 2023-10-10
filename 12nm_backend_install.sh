e4c4037b16f3ddff1324b50cdfbfdd04ed14bc9d
nielsnew <66426672+nielsnew@users.noreply.github.com>
2023-10-10 15:16:13 +0200

    added docker install to bash file

--- 12nm_backend_install.sh
+++ 12nm_backend_install.sh
@@ -7,6 +7,21 @@ sudo apt-get install build-essential manpages-dev python3 git wget gpg -y;
 
 sudo apt install python3-pip; 
 
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh)"; 
 
 sudo apt install curl gnupg apt-transport-https; 
