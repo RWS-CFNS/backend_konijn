43f1f3d21f69892e9606071130acf9be8e42f7f6
nielsnew <nielsadebruin@hotmail.com>
2023-10-11 13:34:55 +0200

    changed greetings example to working restcontroller

--- backend-konijn/src/main/java/nl/cfns/base/SampleController.java
+++ backend-konijn/src/main/java/nl/cfns/base/SampleController.java
@@ -1,15 +1,26 @@
 package nl.cfns.base;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 //a controller like this can be used to display a front-end
 //this example can be accessed through http://localhost:8090/greetings
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//access http://localhost:8090/greeting to see the contents of this controller and send a request
+//access http://localhost:8080/greeting?name=User to see the contents where "user"  is greeted instead
+
 @RestController
 public class SampleController {
+	public record Greeting(long id, String content) {
+	};
 
-    @RequestMapping("/greeting")
-    String hello() {
-        return "HelloWorld!";
-    }
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
