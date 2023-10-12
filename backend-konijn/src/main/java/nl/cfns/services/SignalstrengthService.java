484e0952726434da0516890f0b66bda1412f93aa
nielsnew <66426672+nielsnew@users.noreply.github.com>
2023-10-12 13:05:50 +0200

    added function to filter list of measurements

--- backend-konijn/src/main/java/nl/cfns/services/SignalstrengthService.java
+++ backend-konijn/src/main/java/nl/cfns/services/SignalstrengthService.java
@@ -9,6 +9,7 @@ import nl.cfns.repositories.MeasurementsRepository;
 import nl.cfns.entities.Measurement;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,5 +34,66 @@ public class SignalstrengthService {
 	
     public Map<String, Double> getSignalStrengthHeatmap() throws IOException {
         return generatedHeatmap;
+    }
+  
+    //function to filter based on values given bij the user. if the parameter is null, it indicates that the user does not 
+    //wish to filter based on that member of the entity class
+    public List<Measurement> filterMeasurements(
+            Integer minLatency, Integer maxLatency,
+            Float minUpload, Float maxUpload,
+            Float minDownload, Float maxDownload,
+            Integer minRSSI, Integer maxRSSI,
+            Integer minRSRQ, Integer maxRSRQ,
+            Integer minRSRP, Integer maxRSRP,
+            Integer minSINR, Integer maxSINR,
+            String mnoString,
+            Double minLatitude, Double maxLatitude,
+            Double minLongitude, Double maxLongitude) {
+    	//create List to keep track of valid Measurements
+        List<Measurement> filteredMeasurements = new ArrayList<>();
+
+        //filter based on each member of Measurement class
+        if (minLatency != null && maxLatency != null) {
+            filteredMeasurements.addAll(measurementRepository.findByLatencyBetween(minLatency, maxLatency));
+        }
+
+        if (minUpload != null && maxUpload != null) {
+            filteredMeasurements.addAll(measurementRepository.findByUploadBetween(minUpload, maxUpload));
+        }
+
+        if (minDownload != null && maxDownload != null) {
+            filteredMeasurements.addAll(measurementRepository.findByDownloadBetween(minDownload, maxDownload));
+        }
+
+        if (minRSSI != null && maxRSSI != null) {
+            filteredMeasurements.addAll(measurementRepository.findByRSSIBetween(minRSSI, maxRSSI));
+        }
+
+        if (minRSRQ != null && maxRSRQ != null) {
+            filteredMeasurements.addAll(measurementRepository.findByRSRQBetween(minRSRQ, maxRSRQ));
+        }
+
+        if (minRSRP != null && maxRSRP != null) {
+            filteredMeasurements.addAll(measurementRepository.findByRSRPBetween(minRSRP, maxRSRP));
+        }
+
+        if (minSINR != null && maxSINR != null) {
+            filteredMeasurements.addAll(measurementRepository.findBySINRBetween(minSINR, maxSINR));
+        }
+
+        if (mnoString != null && !mnoString.isEmpty()) {
+            filteredMeasurements.addAll(measurementRepository.findByMnoString(mnoString));
+        }
+
+        if (minLatitude != null && maxLatitude != null) {
+            filteredMeasurements.addAll(measurementRepository.findByLatitudeBetween(minLatitude, maxLatitude));
+        }
+
+        if (minLongitude != null && maxLongitude != null) {
+            filteredMeasurements.addAll(measurementRepository.findByLongitudeBetween(minLongitude, maxLongitude));
+        }
+
+        //return list of valid measurements
+        return filteredMeasurements;
     }
 }
