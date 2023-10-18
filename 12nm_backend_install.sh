4a360aaf4f5ecd6e867562bcc54dbe1aa2df8963
nielsnew <66426672+nielsnew@users.noreply.github.com>
2023-10-18 14:23:21 +0200

    Update 12nm_backend_install.sh

--- 12nm_backend_install.sh
+++ 12nm_backend_install.sh
@@ -1,66 +1,60 @@
 #! /bin/bash
 
-sudo apt-get update; 
+#update current packages and indexes before installations
+sudo apt upgrade; 
+sudo apt update; 
 
-sudo apt-get upgrade; 
-sudo apt-get install build-essential manpages-dev python3 git wget gpg -y; 
 
-sudo apt install python3-pip; 
+#general installations
+sudo apt install -y build-essential manpages-dev python3 git wget gpg npm node; 
+sudo apt install -y curl gnupg apt-transport-https python3-pip;
 
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+#install github desktop
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/kontr0x/github-desktop-install/main/installGitHubDesktop.sh)"; 
 
-sudo apt install curl gnupg apt-transport-https; 
-
+ 
+#download rabbitmq packages using curl package installer
 curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null; 
 
 curl -1sLf "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf77f1eda57ebb1cc" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg > /dev/null; 
 
 curl -1sLf "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/io.packagecloud.rabbitmq.gpg > /dev/null; 
 
+#opdate indexes
 sudo apt update; 
 
-sudo apt install -y erlang-base \ erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \ erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \ erlang-runtime-tools erlang-snmp erlang-ssl \ erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl; 
+#install Erlang programming language packages. Rabbitmq is programmed in Erlang
+sudo apt install -y erlang-base;
+sudo apt install -y erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets;
+sudo apt install -y erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key;
+sudo apt install -y erlang-runtime-tools erlang-snmp erlang-ssl;
+sudo apt install -y erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl;
 
+#install rabbitmq and fix missing packages, this may prevent future errors
 sudo apt install rabbitmq-server -y --fix-missing; 
 
+#start server and enable plugins for management and communications
 sudo systemctl start rabbitmq-server; 
+sudo rabbitmq-plugins enable rabbitmq_shovel rabbitmq_management rabbitmq_management_agent rabbitmq_tracing;
 
-sudo rabbitmq-plugins enable rabbitmq_management; 
-
+#configure users and permissions
 sudo rabbitmqctl add_user cfns stage; 
-
 sudo rabbitmqctl set_user_tags cfns administrator; 
-
 sudo rabbitmqctl set_permissions -p / cfns ".*" ".*" ".*"; 
-
 sudo rabbitmqctl add_user backendUser backendPass; 
-
 sudo rabbitmqctl set_user_tags backendUser administrator; 
-
 sudo rabbitmqctl set_permissions -p / backendUser ".*" ".*" ".*"; 
 
+#install Visual Studio Code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg;
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg;
+sudo install -y -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg;
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list';
 rm -f packages.microsoft.gpg;
+sudo apt install -y code;
 
-sudo apt install default-jre;
-sudo apt install default-jdk;
+#install Java openJDK for backend
+sudo apt install -y default-jre default-jdk;
 
+#update packages and indexes one last time
+sudo apt upgrade; 
 sudo apt update;
-sudo apt install code;
