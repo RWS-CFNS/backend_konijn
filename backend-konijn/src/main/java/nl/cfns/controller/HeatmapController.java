fa6b158397196275d43903a29a279841ff101ceb
Elipam <eliamtraas@gmail.com>
2024-01-09 17:52:40 +0100

    crossorigin stuur naar frontend

--- backend-konijn/src/main/java/nl/cfns/controller/HeatmapController.java
+++ backend-konijn/src/main/java/nl/cfns/controller/HeatmapController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import nl.cfns.service.HeatmapGenerator;
 import nl.cfns.service.SignalstrengthService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/heatmap")
 public class HeatmapController {
 	@Autowired
